0c61e95ccc0cb81378d534d031c932ecd8d04dce
Simon Nikolai Varøy <simon@simonvaroy.xyz>
2025-02-11 15:09:10 +0100

    added oblig 1

--- obliger/oblig1.cpp
+++ obliger/oblig1.cpp
@@ -1,58 +1,161 @@
+#include "LesData2.h"
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// Struct som representerer en oppgave
 struct Oppgave {
-  string beskrivelse, // Hva oppgaven er.
-      ansvarlig;      // Hvem som skal gjøre den.
-  bool erFullfort;    // Om oppgaven er gjort eller ei.
+  string beskrivelse; // Beskrivelse av oppgaven
+  string ansvarlig;   // Person som er ansvarlig for oppgaven
+  bool erFullfort;    // Om oppgaven er fullført eller ikke
 };
 
+// Global vektor som lagrer alle oppgavene
 vector<Oppgave *> gAntallOppgaver;
 
-void skrivMeny() {
-  cout << "Ny oppgave" << endl
-       << "Fjern alle oppgaver" << endl
-       << "Endre status på oppgave" << endl
-       << "Les data på oppgave" << endl
-       << "Skriv data på oppgave" << endl
-       << "Skriv alle oppgaver" << endl
-       << "Skriv meny" << endl;
-}
+// Funksjonsdeklarasjoner (alfabetisk rekkefølge)
+void fjernAlleOppgaver();
+void fjernOppgave();
+void nyOppgave();
+void oppgaveEndreStatus();
+void oppgaveEndreStatus(Oppgave &oppgave);
+void oppgaveLesData(Oppgave &oppgave);
+void oppgaveSkrivData(const Oppgave *oppgave);
+void skrivAlleOppgaver();
+void skrivMeny();
+void skrivOppgave();
 
-void oppgaveLesData(Oppgave &oppgave) {
-  string erFullfortString;
-  cout << "\tBeskrivelse:     ";
-  getline(cin, oppgave.beskrivelse);
-  cout << "\tAnsvarlig:  ";
-  getline(cin, oppgave.ansvarlig);
-  cout << "Er oppgaven fullført?";
-  cin >> erFullfortString;
-  if (erFullfortString == "Ja" || erFullfortString == "ja" ||
-      erFullfortString == "j") {
-    oppgave.erFullfort = true;
-  } else {
-    oppgave.erFullfort = false;
+/**
+ * @brief Fjerner alle oppgaver fra den globale vektoren og frigjør minnet.
+ */
+void fjernAlleOppgaver() {
+  for (int i = 0; i < gAntallOppgaver.size(); i++) {
+    delete gAntallOppgaver[i]; // Slett hver oppgave
   }
+  gAntallOppgaver.clear(); // Tøm vektoren
 }
 
+/**
+ * @brief Fjerner en spesifikk oppgave fra den globale vektoren og frigjør
+ * minnet.
+ * @see lesInt
+ */
+void fjernOppgave() {
+  int nr = lesInt("\tFjerne bok nr", 1,
+                  gAntallOppgaver.size()); // Hent oppgavenummer
+  delete gAntallOppgaver[nr - 1];          // Slett oppgaven
+  gAntallOppgaver[nr - 1] =
+      gAntallOppgaver[gAntallOppgaver.size() -
+                      1];     // Flytt siste oppgave til denne plassen
+  gAntallOppgaver.pop_back(); // Fjern den siste pekeren
+}
+
+/**
+ * @brief Oppretter en ny oppgave og legger den til i den globale vektoren.
+ * @see lesChar
+ */
 void nyOppgave() {
   Oppgave *nyOppgave = new Oppgave;
   cout << "\nNy oppgave:\n";
-  oppgaveLesData(*nyOppgave);
-  gAntallOppgaver.push_back(nyOppgave);
+  oppgaveLesData(*nyOppgave);           // Les oppgavedata
+  gAntallOppgaver.push_back(nyOppgave); // Legg til oppgaven i vektoren
   cout << "\n\tNy oppgave innlagt har nr." << gAntallOppgaver.size() << '\n';
 }
 
-void fjernAlleOppgaver() {
-  for (int i = 0; i < gAntallOppgaver.size();
-       i++) // Sletter ALLE bøkene/struct'ene:
-    delete gAntallOppgaver[i];
-  gAntallOppgaver.clear(); //  Fjerner ALLE PEKERNE i vectoren.
+/**
+ * @brief Endrer fullføringsstatusen til en spesifikk oppgave.
+ * @see lesInt
+ */
+void oppgaveEndreStatus() {
+  int nr = lesInt("Endre status på oppgave nr", 1,
+                  gAntallOppgaver.size()); // Hent oppgavenummer
+  gAntallOppgaver[nr - 1]->erFullfort =
+      !gAntallOppgaver[nr - 1]->erFullfort; // Bytt status
+}
+
+/**
+ * @brief Endrer fullføringsstatusen til en gitt oppgave.
+ * @param oppgave Oppgaven som skal endres.
+ */
+void oppgaveEndreStatus(Oppgave &oppgave) {
+  oppgave.erFullfort = !oppgave.erFullfort; // Bytt status
+}
+
+/**
+ * @brief Leser data for en oppgave fra brukeren.
+ * @param oppgave Oppgaven som skal fylles med data.
+ * @see lesChar
+ */
+void oppgaveLesData(Oppgave &oppgave) {
+  cout << "\tBeskrivelse: ";
+  getline(cin, oppgave.beskrivelse);
+  cout << "\tAnsvarlig: ";
+  getline(cin, oppgave.ansvarlig);
+  char erFullfortChar =
+      lesChar("Er oppgaven fullført? (j/n)");   // Les fullføringsstatus
+  oppgave.erFullfort = (erFullfortChar == 'J'); // Sett status
+}
+
+/**
+ * @brief Skriver ut detaljene for en spesifikk oppgave.
+ * @param oppgave Oppgaven som skal skrives ut.
+ */
+void oppgaveSkrivData(const Oppgave *oppgave) {
+  cout << "\t\t\"" << oppgave->beskrivelse << "\" av " << oppgave->ansvarlig
+       << ((!oppgave->erFullfort) ? " IKKE " : " ") << "fullført\n";
+}
+
+/**
+ * @brief Skriver ut alle oppgavene i den globale vektoren.
+ */
+void skrivAlleOppgaver() {
+  for (int i = 0; i < gAntallOppgaver.size(); i++) {
+    cout << "\tOppgave nr." << setw(2) << i + 1 << ":\n";
+    oppgaveSkrivData(gAntallOppgaver[i]); // Skriv ut hver oppgave
+  }
+}
+
+/**
+ * @brief Skriver ut hovedmenyen med tilgjengelige kommandoer.
+ */
+void skrivMeny() {
+  cout << "Ny oppgave (N)" << endl
+       << "Skriv alle oppgaver (S)" << endl
+       << "Endre status på oppgave (E)" << endl
+       << "Avslutt programmet (Q)" << endl;
+}
+
+/**
+ * @brief Skriver ut en spesifikk oppgave basert på brukerinput.
+ * @see lesInt
+ */
+void skrivOppgave() {
+  int nr = lesInt("\tSe oppgave nr", 1,
+                  gAntallOppgaver.size());   // Hent oppgavenummer
+  oppgaveSkrivData(gAntallOppgaver[nr - 1]); // Skriv ut oppgaven
 }
 
 int main() {
+  char kommando;
+
   skrivMeny();
-  nyOppgave();
+  kommando = lesChar("\nKommando");
+
+  while (kommando != 'Q') {
+    switch (kommando) {
+    case 'N':
+      nyOppgave();
+      break;
+    case 'S':
+      skrivAlleOppgaver();
+      break;
+    case 'E':
+      oppgaveEndreStatus();
+      break;
+    }
+    kommando = lesChar("\nKommando");
+  }
+
+  fjernAlleOppgaver(); // Rydd opp i minnet før avslutning
   return 0;
 }
